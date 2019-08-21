5a85451fae21d5d4ff3a24b079f6423e0f148073
Hoàng Đức Đạt <hddhoangducdat@gmail.com>
2019-08-21 20:52:49 +0700

    Updates

---
+++ precache-manifest.5f47689f2b899c8a322af6bae991480c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1180666d9051db3cfa3869ed4181ca",
+    "url": "/readingspeed/index.html"
+  },
+  {
+    "revision": "3e305ece81c96575b05f",
+    "url": "/readingspeed/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "21f71244c6509a96e018",
+    "url": "/readingspeed/static/js/2.225f5f87.chunk.js"
+  },
+  {
+    "revision": "3e305ece81c96575b05f",
+    "url": "/readingspeed/static/js/main.69710d27.chunk.js"
+  },
+  {
+    "revision": "8c93195e19b754e0e158",
+    "url": "/readingspeed/static/js/runtime~main.dced6d59.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/readingspeed/static/media/logo.5d5d9eef.svg"
+  }
+]);
